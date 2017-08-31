64514249c3dc9641408676ec8600ba57fc51fe0a
PromsingPramod <p.pramodsingh123@gmail.com>
2017-08-31 16:40:01 +0530

    1 commit for git RND

---
+++ GitRNDProject/src/com/git/GitFirstDemoProject.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class GitFirstDemoProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is just a Git Demo Project..");
+
+	}
+
+}
